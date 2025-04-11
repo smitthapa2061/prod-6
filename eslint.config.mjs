5c2248a7252735802e35532de6d3aa26cf4c9189
Smith Thapa <118682095+smitthapa2061@users.noreply.github.com>
2025-04-11 18:53:27 +0545

    u

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Disable unused vars rule
+      "@next/next/no-img-element": "off", // Disable the <img> element warning
+      "react-hooks/exhaustive-deps": "off", // Disable missing dependencies warning in useEffect
+    },
+  },
 ];
 
 export default eslintConfig;
